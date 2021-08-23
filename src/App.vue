0c937035625b0327e0c55faf2d228374e4e20a34
David Docampo <david@docampo.ch>
2021-08-23 23:35:56 +0200

    vue cli installation with tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav>
+        <div>
+            <router-link to="/">Home</router-link>
+            <router-link to="/contact">Contact</router-link>
+        </div>
+    </nav>
+    <router-view></router-view>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <p class="text-red-500 font-semibold">Test</p>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+}
+</script>
